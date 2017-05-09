c0ed489c1011cc79948b8c0dbe7b08fcf7ba1a1c
Rebeca Barcena <rebeca.barcena.o@gmail.com>
2017-05-09 15:54:32 +0200

    Archivos para una web estática de Polaflix

---
+++ WebEstatica/suma.js
@@ -0,0 +1,10 @@
+﻿var valor=0.0;
+var divs = document.getElementById('precio').innerHTML;
+
+for(var i = 0; i < divs.length; i++){
+   //divs[i].innerHTML 
+   valor+=divs[i].innerHTML;
+}
+// realizamos la suma de los valores y los ponemos en la casilla total
+document.getElementById('resultado').innerHTML = valor.toString();
+
