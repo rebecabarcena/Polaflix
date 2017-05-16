7350185650856759afd3adac5af83dde38e52289
Rebeca Barcena <rebeca.barcena.o@gmail.com>
2017-05-16 16:42:17 +0200

    SPA que no funciona

---
+++ PolaflixSPAFallo/polaflix.js
@@ -0,0 +1,19 @@
+﻿var app=angular.module('inicio',['ngRoute']);
+app.config(function($routeProvider){
+      $routeProvider
+          .when("/PantallaInicio",{
+                templateUrl: "PantallaInicio.html",
+            })
+            .when('/VerCargos',{
+                templateUrl: 'VerCargos.html',
+            })
+            .when('/VerSerie',{
+                templateUrl: 'VerSerie.html',
+            })
+            .when('/AgregarSerie',{
+                templateUrl: 'AgregarSerie.html',
+          });
+});
+app.controller('inicioController',function($scope){
+  console.log("Hi, I am breathing");
+});
